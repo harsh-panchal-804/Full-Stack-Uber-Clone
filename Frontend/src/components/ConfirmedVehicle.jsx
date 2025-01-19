49308168e57a730883507f78f3fff6f494e2825e
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-01-19 15:49:26 +0530

    Added demo gif

--- Frontend/src/components/ConfirmedVehicle.jsx
+++ Frontend/src/components/ConfirmedVehicle.jsx
@@ -41,7 +41,7 @@ const ConfirmedVehicle = ({ setConfirmVehiclePanel, setVehicleFound, createRide,
                     setVehicleFound(true)
                     createRide(vehicleType)
                     setConfirmVehiclePanel(false)
-                }}className="w-full bg-slate-900 h-[45px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+                }}className="w-full bg-slate-900 h-[45px]  flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
                    Confirm
                 </button>
                
--- Frontend/src/components/Riding.jsx
+++ Frontend/src/components/Riding.jsx
@@ -16,10 +16,10 @@ const Riding = () => {
             <Link to="/home" className='fixed  right-2 top-2 h-10 rounded-full w-10 flex items-center justify-center bg-white'>
                 <i className="text-xl font-medium ri-home-4-line"></i>
             </Link>
-            <div className="h-[55%] w-full  z-10  relative">
+            <div className="h-[51%] w-full  z-10  relative">
                 <LiveTracking />
             </div>
-            <div className='h-[45%] p-4 bg-slate-300'>
+            <div className='h-[49%] p-4 bg-slate-300'>
                 <div className='flex mt-2 items-center justify-between'>
                     <img className=' h-16 w-22  ' src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png" alt="car" />
                     <div className='text-right'>
@@ -57,7 +57,7 @@ const Riding = () => {
                     setVehicleFound(true)
                     setConfirmVehiclePanel(false)
                 }}
-                 className=" bg-slate-800 w-full  h-[45px] my-5 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
+                 className=" bg-slate-800 w-full my-2  h-[45px]  flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
                     Make a Payment
                 </button>
                
